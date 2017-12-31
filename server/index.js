0c031a6d5d4710a169ad4b6482cec9727c8fed62
tylercable1 <tylercable7@gmail.com>
2017-12-30 21:45:00 -0800

    installed jquery for ajax requests

--- server/index.js
+++ server/index.js
@@ -3,6 +3,21 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 
+var movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
+
+app.get('/movies', (req, res) => {
+  if (err) {
+  	console.log(err);
+  } else {
+  	res.send(movies)
+  }
+})
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
